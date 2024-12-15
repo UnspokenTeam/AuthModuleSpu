32fe7bd38cc7076c30a0cbc16f0c4509873014e2
MMVlasko <144661668+MMVlasko@users.noreply.github.com>
2024-12-15 18:21:55 +0300

    SPUT-18: feature/added registration endpoint (#4)
    * feat: added registration endpoint
    
    * fix: email validator and exception name
    
    * fix: validator and exceptions

---
+++ src/AuthModuleSpu.Application/Commands/Auth/CreateUser/CreateUserCommandHandler.cs
@@ -0,0 +1,32 @@
+﻿using System.Text.Json;
+using AuthModuleSpu.Application.Commands.Auth.CreateUser.Contracts;
+using AuthModuleSpu.Application.Commands.Auth.CreateUser.Contracts.Mappers;
+using AuthModuleSpu.Application.Exceptions.Auth.BadValue;
+using AuthModuleSpu.Application.Validators.Auth.Email;
+
+using AuthModuleSpu.Infrastructure.Repository.Auth;
+using MediatR;
+
+
+namespace AuthModuleSpu.Application.Commands.Auth.CreateUser;
+
+public class CreateUserCommandHandler
+(
+    IAuthRepository authRepository    
+) : IRequestHandler<CreateUserCommand, CreateUserCommandResponse>
+{
+    public async Task<CreateUserCommandResponse> Handle(CreateUserCommand request, CancellationToken cancellationToken)
+    {
+        EmailValidator.ValidateEmail(request.Email, request);
+            
+        var created = await authRepository.CreateUserAsync(CreateUserCommandMapper.ToInternal(request));
+
+        if (!created)
+        {
+            throw new BadValueException("User with such data already exists", 
+                JsonSerializer.Serialize(request));
+        }
+        
+        return new CreateUserCommandResponse();
+    }
+}
